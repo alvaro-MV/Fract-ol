7c45e43da7e5ed0da3159969514588433017075f
Álvaro Morales <moralesalvaro97@gmail.com>
2024-08-25 00:48:27 +0200

    not printing line

--- exploring_minilib.c
+++ exploring_minilib.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "lib/include/libft.h"
+#include <math.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -14,6 +15,10 @@ typedef struct	s_data {
 typedef	struct var_mlx {
 	void	*win;
 	void	*mlx;
+	t_data	img;
+	int		height;
+	int		width;
+	int		height_offset;
 }			mlx_vars;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -40,36 +45,23 @@ void	print_circle(int centrus, int radius, int color, t_data img)
 		i++;
 	}
 }
-#include <limits.h>
-void	add_shade(double distance, int *color)
-{
-	int map;
 
-	if (distance < 0 || distance > 1)
-		return ;
-	map = (int) distance * 0x100;
-	map = ((0xFF * map) / 0x100) << 24;
-	*color = *color & 0xFFFFFF;
-	*color = *color + map;	
-}
-
-int	close_win(int	keymap, mlx_vars *vars)
+int	print_line(int keymap, mlx_vars *vars)
 {
-	if (keymap == 65307)
-		mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}
+	int	j = 0;
 
-int	close_win_cross(int x, int y, mlx_vars *vars)
-{
-	write(1, "HOOOOOOLLLAA", 6);
-	//ft_printf("location  x: %d  y: %d\n", x, y);
-	return (0);
-}
-
-int	window_resize(mlx_vars *vars)
-{
-	write(1, "window resized", 15);
+	if (keymap == 65364)
+	{
+		while (j < vars->width)
+		{
+			//ft_printf("column: %d\n", j);
+			my_mlx_pixel_put(&vars->img, vars->height_offset, j, 0x00FF0000);
+			j++;
+		}
+		mlx_put_image_to_window(vars->mlx, vars->win, &vars->img, 0, 0);
+		vars->img.img = mlx_new_image(vars->mlx, 900, 800);
+		vars->height_offset++;
+	}
 	return (0);
 }
 
@@ -79,23 +71,15 @@ int	main(void)
 	t_data		img;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	vars.height = 800;
+	vars.width = 900;
+	vars.height_offset = 0;
+	vars.win = mlx_new_window(vars.mlx, 900, 800, "Hello world!");
+	img.img = mlx_new_image(vars.mlx, 900, 800);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-//	int	radius =20000;
-	//int	centrus = 600;
-	//int color = 0x00904428;
-	//double	i = 10;
-	//while (i--)
-	//{
-		////add_shade(i/10, &color);
-		//color = color + 0x07000000;
-		//radius -= 100;
-		//print_circle(centrus, radius, color, img);
-	//}
-	//mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_hook(vars.win, 4, 1L<<2, close_win_cross, &vars);
+	vars.img = img;
+	mlx_key_hook(vars.win, print_line, &vars);
 	mlx_loop(vars.mlx);
 }
 
